eebdb7e517d3d395dd816487edd1e715bdf255f1
2eYJ <qpalamz2@gmail.com>
2021-01-12 19:05:11 +0900

    STEP1 cout cin 객체 공부함

---
+++ 1STEP/ex2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //cout 
+    cout << "C++ Study ~~!" << endl;
+    
+    
+    int age;
+    cout << "how old r u? : ";
+    cin >> age;
+
+    cout << "you r  " << age << " old.";
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 2021.01 ~
 
 vscode C/C++ 코딩 환경 구축 하는 법 https://www.youtube.com/watch?v=3-PD_AUSOLM
-
