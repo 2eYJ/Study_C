d63422a40c3d59289ce822a803f6374ed66f6fed
2eYJ <qpalamz2@gmail.com>
2021-01-13 21:33:00 +0900

    변수 공부함

---
+++ 2STEP/ex1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //변수 선언만 하는 방법
+    int num1;
+    //변수 선언 함 메모리 할당 받음 메모리안에는 가비지 값이 들어감
+    //초기와하지 않는 변수는 절대로 사용해서는 안 됩니다.
+
+    num1 = 11;  // 변수 초기화
+    cout << num1 << endl;
+
+    double num2 = 2.2;
+    cout << num2;   
+
+}
